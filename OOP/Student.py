3c0bd43d30c2d6b0cc9b1f521c00818de18f17a1
Connor <xiaobang199806@163.com>
2023-04-12 10:50:33 +0800

    add student

---
+++ OOP/Student.py
@@ -0,0 +1,18 @@
+# 定义学生对象
+class Student(object):
+    # 属性
+    def __init__(self, name, age, gender):
+        self.__name = name
+        self.__age = age
+        self.__gender = gender
+
+    # 方法和接口
+    def __str__(self):
+        stu_info = f'姓名：{self.__name} 年龄：{self.__age} 性别：{self.__gender}'
+        return stu_info
+
+
+# 测试该模块
+if __name__ == '__main__':
+    stu = Student('Tom', 20, 'male')
+    print(stu)
